ca5515f800845ac099207f50666beca62ee1ffb8
ruucm <ruucm.a@gmail.com>
2020-11-13 14:24:22 +0900

    fix: Home
    remove animate

--- documentation-site/pages/home/home.tsx
+++ documentation-site/pages/home/home.tsx
@@ -1,22 +1,22 @@
-import { motion } from "framer-motion"
-import { NextSeo } from "next-seo"
-import Link from "next/link"
-import * as React from "react"
-import * as System from "../../../../lighthouse"
-import { useTheme } from "../../../../lighthouse"
-import * as Asset from "../../assets"
-import Layout from "../../components/layout"
-import { githubUrl } from "../../constants"
-import { DesktopView, MobileView, sizeBg } from "../../utils"
-import { Heading } from "./styled-components"
+import { motion } from "framer-motion";
+import { NextSeo } from "next-seo";
+import Link from "next/link";
+import * as React from "react";
+import * as System from "../../../../lighthouse";
+import { useTheme } from "../../../../lighthouse";
+import * as Asset from "../../assets";
+import Layout from "../../components/layout";
+import { githubUrl } from "../../constants";
+import { DesktopView, MobileView, sizeBg } from "../../utils";
+import { Heading } from "./styled-components";
 
 const headingText = `Lighthouse is a continuous design system for the design and development workflows. You can design with real components and develop in the same way using the power of Framer.
 
-Lighthouse Design System also provides fully customizable theme.`
+Lighthouse Design System also provides fully customizable theme.`;
 
 export default function HomePage() {
-  const theme = useTheme()
-  const sharedProps = { $theme: theme }
+  const theme = useTheme();
+  const sharedProps = { $theme: theme };
   return (
     <Layout home>
       <Heading {...sharedProps}>
@@ -55,7 +55,6 @@ export default function HomePage() {
               href={githubUrl}
               target="_blank"
               rel="noreferrer"
-              animate
               startEnhancer={<Asset.Github height={20} color={theme.colors.primary} />}
               overrides={{
                 BaseButton: {
@@ -70,5 +69,5 @@ export default function HomePage() {
         </System.FlexBox>
       </Heading>
     </Layout>
-  )
+  );
 }
