af3b1d45fbda80bc09c23a8d67818e0ff9047247
ruucm <ruucm.a@gmail.com>
2020-11-13 19:35:58 +0900

    design: update cover

--- lighthouse-gallery.framerfx/code/canvas.tsx
+++ lighthouse-gallery.framerfx/code/canvas.tsx
@@ -1,10 +1,10 @@
 // WARNING: this file is auto generated, any changes will be lost
 import { createDesignComponent, CanvasStore } from "framer"
-const canvas = CanvasStore.shared() // CANVAS_DATA;
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
 
 export const colors = Object.freeze({
-  /** rgba(178, 178, 178, 0) */
-  guideBg: "var(--token-9dbbf204-255f-4d80-8289-3c093afbd52b, rgba(178, 178, 178, 0))",
-  /** rgba(196, 222, 183, 0) */
-  sizeBg: "var(--token-79b72d1c-dc07-424f-a8b9-9d178194c29a, rgba(196, 222, 183, 0))",
+    /** rgba(178, 178, 178, 0) */
+    "guideBg": "var(--token-9dbbf204-255f-4d80-8289-3c093afbd52b, rgba(178, 178, 178, 0))",
+    /** rgba(196, 222, 183, 0) */
+    "sizeBg": "var(--token-79b72d1c-dc07-424f-a8b9-9d178194c29a, rgba(196, 222, 183, 0))",
 })
