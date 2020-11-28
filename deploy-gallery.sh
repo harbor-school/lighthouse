76282dab23dcab1a1b0cf54b7c9bba9b64a054f3
ruucm <ruucm.a@gmail.com>
2020-11-28 13:03:28 -0800

    feat: deploy-gallery
    trim scripts

--- deploy-gallery.sh
+++ deploy-gallery.sh
@@ -1,20 +1,25 @@
+# Duplicate Gallery
+cp -r lighthouse-gallery.framerfx lighthouse-gallery-deploy.framerfx
+
 # Remove Project code files "except overrides" directory
-find ./lighthouse-gallery.framerfx/code/* -maxdepth 0 -name 'overrides' -prune -o -exec rm -rf '{}' ';'
+find ./lighthouse-gallery-deploy.framerfx/code/* -maxdepth 0 -name 'overrides' -prune -o -exec rm -rf '{}' ';'
 
 # String find & replace (component files)
-find ./lighthouse-gallery.framerfx -type f -exec sed -i "" 's/..\/..\/..\/..\/lighthouse/@harborschool\/lighthouse/g' {} \;
-find ./lighthouse-gallery.framerfx -type f -exec sed -i "" 's/..\/..\/..\/lighthouse/@harborschool\/lighthouse/g' {} \;
-find ./lighthouse-gallery.framerfx -type f -exec sed -i "" 's/from "..\/utils/from "@framer\/ruucm-h.lighthouse\/code\/utils/g' {} \;
+find ./lighthouse-gallery-deploy.framerfx -type f -exec sed -i "" 's/..\/..\/..\/..\/lighthouse/@harborschool\/lighthouse/g' {} \;
+find ./lighthouse-gallery-deploy.framerfx -type f -exec sed -i "" 's/..\/..\/..\/lighthouse/@harborschool\/lighthouse/g' {} \;
+find ./lighthouse-gallery-deploy.framerfx -type f -exec sed -i "" 's/from "..\/utils/from "@framer\/ruucm-h.lighthouse\/code\/utils/g' {} \;
 # String find & replace (document.json)
-find ./lighthouse-gallery.framerfx -type f -exec sed -i "" 's/"codeComponentIdentifier" : "./"codeComponentIdentifier" : "@framer\/ruucm-h.lighthouse\/./g' {} \;
+find ./lighthouse-gallery-deploy.framerfx -type f -exec sed -i "" 's/"codeComponentIdentifier" : "./"codeComponentIdentifier" : "@framer\/ruucm-h.lighthouse\/./g' {} \;
 # String find & replace (package.json)
-find ./lighthouse-gallery.framerfx -type f -exec sed -i "" 's/"name": "@framer\/ruucm-h.lighthouse",//g' {} \;
-find ./lighthouse-gallery.framerfx -type f -exec sed -i "" 's/"dependencies": {/"dependencies": {\
+find ./lighthouse-gallery-deploy.framerfx -type f -exec sed -i "" 's/"name": "@framer\/ruucm-h.lighthouse",//g' {} \;
+find ./lighthouse-gallery-deploy.framerfx -type f -exec sed -i "" 's/"dependencies": {/"dependencies": {\
     "@harborschool\/lighthouse": "latest",/g' {} \;
 
+# Open Framer
+open lighthouse-gallery-deploy.framerfx
 
 #TODO
-# Install @framer/ruucm-h.lighthouse
+# Install @framer/ruucm-h.lighthouse (Framer Package)
 # Remove scaleChip (Project Component)
 # Save it as .framerx project
-# Upload lighthouse-gallery.framerx to Framer Web
+# Upload lighthouse-gallery-deploy.framerx to Framer Web
