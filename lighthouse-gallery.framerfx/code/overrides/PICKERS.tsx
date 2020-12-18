b20d10a32a1664714608832c07b26738afc034ab
ruucm <ruucm.a@gmail.com>
2020-12-17 16:05:56 -0800

    framer: Select
    update override codes

--- lighthouse-gallery.framerfx/code/overrides/PICKERS.tsx
+++ lighthouse-gallery.framerfx/code/overrides/PICKERS.tsx
@@ -8,39 +8,38 @@ export function Select(): Override {
   const theme = getThemeByKey(getCurrentTheme())
   const [isOpen, setIsOpen] = useState(false)
   const [active, setActive] = useState(0)
+  const options = [
+    { label: "label", endEnhancer: <div>🤢</div> },
+    {
+      label: "label2",
+      endEnhancer: (
+        <System.Icons.Lock width={theme.sizing.scale600} color={theme.colors.contentSecondary} />
+      ),
+    },
+    {
+      label: "label3",
+      endEnhancer: <System.Icons.Lock width={theme.sizing.scale600} color={theme.colors.primary} />,
+    },
+    {
+      label: "label4",
+      endEnhancer: <System.Icons.Menu width={theme.sizing.scale600} color="red" />,
+    },
+    {
+      label: "Disabled Label",
+      disabled: true,
+      endEnhancer: (
+        <System.Icons.Lock width={theme.sizing.scale600} color={theme.colors.contentTertiary} />
+      ),
+    },
+  ]
 
   return {
-    placeholder: "Yap!",
+    placeholder: options[active].label,
     isOpen,
     active,
     onClick: () => setIsOpen(true),
     onClickOutside: () => setIsOpen(false),
-    options: [
-      { label: "label", endEnhancer: <div>🤢</div> },
-      {
-        label: "label2",
-        endEnhancer: (
-          <System.Icons.Lock width={theme.sizing.scale600} color={theme.colors.contentSecondary} />
-        ),
-      },
-      {
-        label: "label3",
-        endEnhancer: (
-          <System.Icons.Lock width={theme.sizing.scale600} color={theme.colors.primary} />
-        ),
-      },
-      {
-        label: "label4",
-        endEnhancer: <System.Icons.Menu width={theme.sizing.scale600} color="red" />,
-      },
-      {
-        label: "Disabled Label",
-        disabled: true,
-        endEnhancer: (
-          <System.Icons.Lock width={theme.sizing.scale600} color={theme.colors.contentTertiary} />
-        ),
-      },
-    ],
+    options,
     onChange: (value: any) => {
       setActive(value)
       setIsOpen(false)
