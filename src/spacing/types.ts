5acecb9aefd4bfa4fcd7e3b34a52bdd30f1d0d31
ruucm <ruucm.a@gmail.com>
2021-02-08 14:21:25 +0900

    feat: Spacing
    add display prop type

--- src/spacing/types.ts
+++ src/spacing/types.ts
@@ -1,7 +1,9 @@
 import sizing from "../themes/shared/sizing"
+import * as System from "../lighthouse"
 
 export type SpacingPropsT = {
   width?: keyof typeof sizing
   height?: keyof typeof sizing
   backgroundColor?: string
+  display?: System.BlockPropsT["display"]
 }
