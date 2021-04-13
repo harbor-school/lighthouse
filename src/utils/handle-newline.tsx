bd3f7394caa670caf366837face18e3e02ac04a6
ruucm <ruucm.a@gmail.com>
2021-04-14 02:16:54 +0900

    fix: Typography
    rm default <br />

--- src/utils/handle-newline.tsx
+++ src/utils/handle-newline.tsx
@@ -2,11 +2,6 @@ import * as React from "react"
 
 export const handleNewline = (text) => {
   if (typeof text === "string")
-    return text.split("\n").map((item, key) => (
-      <React.Fragment key={key}>
-        {item}
-        <br />
-      </React.Fragment>
-    ))
+    return text.split("\n").map((item, key) => <React.Fragment key={key}>{item}</React.Fragment>)
   else return text
 }
