11d00f977ab8db8801e78abfb9e7ccf46dfb1562
ruucm <ruucm.a@gmail.com>
2021-07-25 22:48:00 +0900

    fix: spacing

--- lighthouse-gallery.framerfx/code/Spacing.tsx
+++ lighthouse-gallery.framerfx/code/Spacing.tsx
@@ -10,7 +10,7 @@ const InnerSpacing = (props) => {
       width={props.widthAmount}
       height={props.heightAmount}
       backgroundColor={"var(--lh-guide-spacing)"}
-      overrides={{ Block: { opacity: "var(--lh-guide-spacing-opacity)" } }}
+      overrides={{ Block: { opacity: "var(--lh-guide-opacity)" } }}
     ></System.Spacing>
   )
 }
